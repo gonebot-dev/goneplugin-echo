e6278d195639a9c7bf51fddd36c3c60bc9025edd
int16 <int16int8@gmail.com>
2024-08-27 07:41:37 +0000

    init: Create Echo plugin.

---
+++ echo.go
@@ -0,0 +1,30 @@
+package echo
+
+import (
+	"encoding/json"
+	"log"
+
+	"github.com/gonebot-dev/gonebot/messages"
+	"github.com/gonebot-dev/gonebot/plugins"
+)
+
+func echoHandler(msg messages.MessageStruct) messages.ResultStruct {
+	result := messages.ResultStruct{}
+	result.Text = msg.Text
+
+	dResult, _ := json.Marshal(result)
+	log.Printf("Echo: %s\n", dResult)
+
+	return result
+}
+func init() {
+	echo := plugins.GonePlugin{}
+	echo.Name = "Echo"
+	echo.Description = "Echo text message."
+	echo.Handlers = append(echo.Handlers,
+		plugins.GoneHandler{
+			Command: []string{"echo"},
+			Handler: echoHandler,
+		})
+	plugins.LoadPlugin(echo)
+}
---
+++ go.mod
@@ -0,0 +1,7 @@
+module github.com/gonebot-dev/goneplugin-echo
+
+go 1.22.6
+
+require github.com/gonebot-dev/gonebot v0.1.1
+
+require github.com/joho/godotenv v1.5.1 // indirect
