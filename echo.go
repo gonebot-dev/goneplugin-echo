dc1acffa8b1327ca1d8f0f4742f230253a6b4916
Kingcq <404291187@qq.com>
2024-09-18 10:44:39 +0800

    feat: update to gonebot v1

--- echo.go
+++ echo.go
@@ -1,29 +1,27 @@
 package echo
 
 import (
-	"encoding/json"
-	"log"
-
-	"github.com/gonebot-dev/gonebot/messages"
-	"github.com/gonebot-dev/gonebot/plugins"
+	"github.com/gonebot-dev/gonebot/adapter"
+	"github.com/gonebot-dev/gonebot/message"
+	"github.com/gonebot-dev/gonebot/plugin"
+	"github.com/gonebot-dev/gonebot/rule"
 )
 
-func echoHandler(msg messages.IncomingStruct) (result messages.ResultStruct) {
-	result.Text = msg.Text
+var Echo plugin.GonePlugin
 
-	dResult, _ := json.Marshal(result)
-	log.Printf("Echo: %s\n", dResult)
-
-	return result
-}
 func init() {
-	echo := plugins.GonePlugin{}
-	echo.Name = "Echo"
-	echo.Description = "Echo text message."
-	echo.Handlers = append(echo.Handlers,
-		plugins.GoneHandler{
-			Command: []string{"echo"},
-			Handler: echoHandler,
-		})
-	plugins.LoadPlugin(echo)
+	Echo.Name = "Echo"
+	Echo.Version = "v0.0.1"
+	Echo.Description = "Reply the same message of what you have sent"
+
+	Echo.Handlers = append(Echo.Handlers, plugin.GoneHandler{
+		Rules: []rule.FilterBundle{{Filters: []rule.FilterRule{rule.ToMe(), rule.Command([]string{"echo"})}}},
+		Handler: func(a *adapter.Adapter, msg message.Message) bool {
+			reply := msg
+			reply.Sender = reply.Self
+			reply.Receiver = msg.Sender
+			a.SendChannel.Push(reply, false)
+			return true
+		},
+	})
 }
--- go.mod
+++ go.mod
@@ -1,7 +1,7 @@
 module github.com/gonebot-dev/goneplugin-echo
 
-go 1.22.6
+go 1.22.7
 
-require github.com/gonebot-dev/gonebot v0.1.2
+require github.com/gonebot-dev/gonebot v0.1.5-0.20240918023545-a943c9502ac6
 
 require github.com/joho/godotenv v1.5.1 // indirect
