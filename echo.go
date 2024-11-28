8cae1cd2ea7b406b1c3feef2a2742c31108bce5f
Kingcq <404291187@qq.com>
2024-11-28 16:35:29 +0800

    feat: updated gonebot version to v1.0.2

--- echo.go
+++ echo.go
@@ -11,14 +11,13 @@ var Echo plugin.GonePlugin
 
 func init() {
 	Echo.Name = "Echo"
-	Echo.Version = "v0.0.1"
+	Echo.Version = "v0.1.0"
 	Echo.Description = "Reply the same message of what you have sent"
 
 	Echo.Handlers = append(Echo.Handlers, plugin.GoneHandler{
 		Rules: rule.NewRules(rule.ToMe()).And(rule.Command("echo")),
 		Handler: func(a *adapter.Adapter, msg message.Message) bool {
-			reply := message.NewReply(msg)
-			reply.AttachMessages(msg)
+			reply := message.NewReply(msg).Join(msg)
 			a.SendMessage(reply)
 			return true
 		},
